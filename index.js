07c61fd4f49e35ed83ea1f9e131f946f637fc1a3
Augusto Varandas <augustovarandas21@gmail.com>
2024-01-09 19:38:06 -0300

    Melhora menu responsivo

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 function clickMenu() {
-  // const itens = document.querySelector("#itens");
-  const itens = window.itens;
+  const itens = document.querySelector("#itens");
+  // const itens = window.itens;
   if (itens.style.display == 'none') {
     itens.style.display = 'flex'
   } else {
     itens.style.display = 'none'
+  }
+}
+
+function fecharMenu() {
+  const itens = document.querySelector("#itens");
+  // const itens = window.itens;
+  if (itens.style.display == 'flex') {
+    itens.style.display = 'none'
+  } else {
+    itens.style.display = 'flex'
   }
 }
